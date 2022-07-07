a88a7832166a859d8a7707d195f0ae57dfdae090
arman672 <ahmedarman672@gmail.com>
2022-07-07 17:42:48 +0530

    authentication and authorization done

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,15 +1,51 @@
 const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
+const bookModel = require("../models/bookModel");
 
 //===================================================[API:FOR AUTHENTICATION]===========================================================
-exports.authentication = async (req, res, next) => {
+exports.authentication = async function (req, res, next) {
     try {
         let token = req.headers["x-api-key"] || req.headers["X-API-KEY"]
         if (!token) {
             return res.status(401).send({ status: false, message: "token must be present" })
         }
         let decodedtoken = jwt.verify(token, 'lama');
-        req.loggedInUser = decodedtoken.userId
+        req.loggedInUserId = decodedtoken.userId
         next();
+    }
+    catch (err) { return res.status(500).send({ status: false, message: err.message }); }
+}
+
+//===================================================[API:FOR AUTHORIZATION]===========================================================
+exports.authorization = async function (req, res, next) {
+    try {
+        //for update delete
+        const idInParams = req.params.bookId
+        if (!mongoose.isValidObjectId(req.params.bookId)) {
+            return res.status(400).send({ status: false, message: "enter valid userId" })
+        }
+        if (idInParams) {
+            const bookOwner = await bookModel.findOne({ _id: idInParams })
+            if (req.loggedInUserId == bookOwner.userId) {
+                console.log("hello")
+                return next();
+            } else {
+                return res.status(403).send({ status: false, message: "you you are not authroized to perform this operation" });
+            }
+        }
+
+        //for create
+        const idInBody = req.body.userId
+        if (!mongoose.isValidObjectId(idInBody)) {
+            return res.status(400).send({ status: false, message: "enter valid userId" })
+        }
+        if (idInBody) {
+            if (req.loggedInUserId == idInBody) {
+                return next();
+            } else {
+                return res.status(403).send({ status: false, message: "you are not authroized to perform this operation" });
+            }
+        }
     }
     catch (err) { return res.status(500).send({ status: false, message: err.message }); }
 }
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,14 +1,16 @@
 const express = require('express')
 const bookController= require('../controllers/bookController')
 const userController = require('../controllers/userController')
+const mw = require('../middlewares/auth')
 const router = express.Router()
 
 router.post("/register", userController.registerUser);
 router.post("/login", userController.loginUser);
 
-router.post("/books", bookController.createBook);
-router.get("/books", bookController.getBook);
-router.get("/books/:bookId", bookController.bybookId);
+router.post("/books", mw.authentication,mw.authorization, bookController.createBook);
+router.get("/books", mw.authentication,mw.authorization, bookController.getBook);
+router.get("/books/:bookId", mw.authentication,mw.authorization, bookController.bybookId);
+router.put("/books/:bookId", mw.authentication,mw.authorization, bookController.updateBook);
 
 
 module.exports=router
